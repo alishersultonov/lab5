ed7f1b5dc743a819b637bc9ff1ccca366f4b488a
Alisher Sultonov <a.sultonov@newuu.uz>
2025-02-13 10:06:54 +0500

    Lab5

--- main.cpp
+++ main.cpp
@@ -297,6 +297,27 @@ int main() {
 }
 */
 
+
+//Problem9
+int main() {
+    int num;
+    bool isPrime = true;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (num <= 1) isPrime = false;
+    else {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime) cout << "Number is prime" << endl;
+    else cout << "Number is not prime" << endl;
+    return 0;
+}
+
 /*
 //Problem
 int main() {
